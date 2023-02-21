d1b1b3c07c7bade23f5b0991d0176dd9293d4394
DD-RR <mr.diego931@gmail.com>
2023-02-20 18:35:22 -0600

    first Comit

---
+++ TheMovie-iOS/TheMovie-iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TheMovie-iOS
+//
+//  Created by Diego David Rodríguez Reyna on 20/02/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
